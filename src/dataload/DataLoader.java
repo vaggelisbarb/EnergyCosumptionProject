d7f8faa5949dc03603f6584b4325808555e160c0
vaggelisbarb <vaggelis21aek@hotmail.com>
2019-12-03 00:30:23 +0200

    Kitchen-Laundry-AC_Loader deleted

--- src/dataload/DataLoader.java
+++ src/dataload/DataLoader.java
@@ -2,48 +2,46 @@ package dataload;
 
 import java.util.ArrayList;
 
+
+import datamodel.MeasurementRecord;
+import datamodel.ResultManager;
+
 public class DataLoader implements ILoader{
 	private String filename;
 	
-	private KitchenLoader kitchenloader;
-	private LaundryLoader laundryloader;
-	private AcLoader acloader;
-	
-	/**
-	 * @param filename
-	 * @param kitchenloader
-	 * @param laundryloader
-	 * @param acloader
-	 */
-	public DataLoader(String filename, KitchenLoader kitchenloader, LaundryLoader laundryloader, AcLoader acloader) {
-		super();
-		this.filename = filename;
-		this.kitchenloader = kitchenloader;
-		this.laundryloader = laundryloader;
-		this.acloader = acloader;
-	}
-	
 
-	/**
-	 * @param filename
-	 */
+	public DataLoader() {
+		super();
+	}
+
 	public DataLoader(String filename) {
 		super();
 		this.filename = filename;
 	}
 
-	public DataLoader() {
-		filename = "/home/vaggelisbarb/Eclipse_Projects/2019_2020_<2766>_<2784>_<2821>/Resources/TestInput/household_preview.txt";
-		kitchenloader = new KitchenLoader();
-		laundryloader = new LaundryLoader();
-		acloader = new AcLoader();
-		
-	}
-
-
 	@Override
 	public int load(String fileName, String delimeter, boolean hasHeaderLine, int numFields, ArrayList objCollection) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	
+	public int constructRecordsFromRow(String[] tokens, ResultManager resultmanager) {
+		String fullDate;
+		String fullTime;
+		double kitchenCons;
+		double laundryCons;
+		double acCons;
+		
+		fullDate = tokens[0];
+		fullTime = tokens[1];
+		kitchenCons = Double.parseDouble(tokens[6]);
+		laundryCons = Double.parseDouble(tokens[7]);
+		acCons = Double.parseDouble(tokens[8]);
+		
+		MeasurementRecord kiObj = new MeasurementRecord(fullDate, fullTime, kitchenCons, 0, 0);
+		MeasurementRecord laObj = new MeasurementRecord(fullDate, fullTime, 0, laundryCons, 0);
+		MeasurementRecord acObj = new MeasurementRecord(fullDate, fullTime, 0, 0, acCons);
+		
+		resultmanager.add()
 	}
 }
