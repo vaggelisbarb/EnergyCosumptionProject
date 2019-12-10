b6d00cb935d4e5325b4039352734baa2203117fa
vaggelisbarb <vaggelis21aek@hotmail.com>
2019-12-10 22:08:12 +0200

    Load() testing is passed

--- src/datamodel/ResultManager.java
+++ src/datamodel/ResultManager.java
@@ -3,24 +3,14 @@
  */
 package datamodel;
 
-import static org.hamcrest.CoreMatchers.nullValue;
 
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
-import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.time.format.TextStyle;
 import java.time.temporal.ChronoField;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 
-import javax.swing.text.DateFormatter;
 
 
 
@@ -29,157 +19,210 @@ import javax.swing.text.DateFormatter;
  *
  */
 public class ResultManager implements IResult{
-	private ArrayList<MeasurementRecord> RecordsList;
-	
+	private HashMap<String, ArrayList<MeasurementRecord>> recordsHashMap;
+	private String description;
+	private String aggFunction;
+	private String timeUnit;
   	
 	public ResultManager() {
-		RecordsList = new ArrayList<MeasurementRecord>();
+		recordsHashMap = new HashMap<String, ArrayList<MeasurementRecord>>(); 
 	}
-	
+
 	// Convert date from input String to Date object	
 	public LocalDateTime convertStringToDate(String timeUnit) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy,HH:mm:ss");
 		LocalDateTime dateTime = LocalDateTime.parse(timeUnit, formatter);
 		return dateTime;
 	}
-
-	public String checkTimeUnit(String timeUnit) {
-		if(timeUnit.equals("WINTER") || timeUnit.equals("SUMMER") || timeUnit.equals("AUTUMN") || timeUnit.equals("SPRING"))
-			return "Season";
-		else if(timeUnit.equals("MONDAY") || timeUnit.equals("TUESDAY") || timeUnit.equals("WEDNESDAY") || timeUnit.equals("THURSDAY") || timeUnit.equals("FRIDAY") || timeUnit.equals("SATURDAY") || timeUnit.equals("SUNDAY"))
-			return "Day Of Week";
-		else if(timeUnit.equals("JAN") || timeUnit.equals("FEB") || timeUnit.equals("MAR") || timeUnit.equals("APR") || timeUnit.equals("MAY") || timeUnit.equals("JUN") || timeUnit.equals("JUL") || timeUnit.equals("AUG") || timeUnit.equals("SEP") || timeUnit.equals("OCT") || timeUnit.equals("NOV") || timeUnit.equals("DEC"))
-			return "Month";
-		else if(timeUnit.equals("NIGHT")||timeUnit.equals("EARLY_MORNING")||timeUnit.equals("AFTERNOON")||timeUnit.equals("MORNING")||timeUnit.equals("EVENING"))
-			return "DayPeriod";
+	public String[] isSeason(String timeUnit) {
+		
+		switch (timeUnit) {
+		case "WINTER":
+			String[] winterArr = {"12","1","2"};
+			return winterArr;
+		case "SUMMER" :
+			String[] summerArr = {"6","7","8"};
+			return summerArr;
+		case "SPRING":
+			String[] springArr = {"3","4","5"};
+			return springArr;
+		case "AUTUMN":
+			String[] autumnArr = {"9","10","11"};
+			return autumnArr;
+		case "SEASON":
+			String[] seasonArr = {"1","2","3","4","5","6","7","8","9","10","11","12"};
+			return seasonArr;
+		}
+		return null;
+		
+	}
+	
+	public String[] isWeek(String timeUnit) {
+		switch (timeUnit) {
+		case "MONDAY":
+			String[] monArr= {"1"}; 
+			return monArr;
+		case "TUESDAY":
+			String[] tueArr= {"2"}; 
+			return tueArr;
+		case "WEDNESDAY":
+			String[] wedArr= {"3"}; 
+			return wedArr;
+		case "THURSDAY":
+			String[] thuArr= {"4"}; 
+			return thuArr;
+		case "FRIDAY":
+			String[] friArr= {"5"}; 
+			return friArr;
+		case "SATURDAY":
+			String[] satArr= {"6"}; 
+			return satArr;	
+		case "SUNDAY":
+			String[] sunArr= {"7"}; 
+			return sunArr;
+		case "WEEK" :
+			String[] weekArr= {"1","2","3","4","5","6","7"};
+			return weekArr;
+		}
 		return null;
 	}
 	
-	// Checks if timUnit is season
-	public int isSeason(String timeUnit,LocalDateTime fullDate) {
-		int monthInt = fullDate.getMonthValue();
-	  	
+	public String[] isMonth(String timeUnit) {
 		switch (timeUnit) {
-		case "WINTER":
-			if(monthInt==12 |monthInt==1 || monthInt==2)
-				return 1;
-			return 0;
-		case "SPRING" :
-			if(monthInt==3 |monthInt==4 || monthInt==5)
-				return 1;
-			return 0;
-		case "SUMMER" :
-			if(monthInt==6 |monthInt==7 || monthInt==8)
-				return 1;
-			return 0;
-		case "AUTUMN" :
-			if(monthInt==9 |monthInt==10 || monthInt==11)
-				return 1;
-			return 0;
+		case "JAN":
+			String[] janArr= {"1"}; 
+			return janArr;
+		case "FEB":
+			String[] febArr= {"2"}; 
+			return febArr;
+		case "MAR":
+			String[] marArr= {"3"}; 
+			return marArr;
+		case "APR":
+			String[] aprArr= {"4"}; 
+			return aprArr;
+		case "MAY":
+			String[] mayArr= {"5"}; 
+			return mayArr;
+		case "JUN":
+			String[] junArr= {"6"}; 
+			return junArr;	
+		case "JUL":
+			String[] julArr= {"7"}; 
+			return julArr;
+		case "AUG" :
+			String[] augArr= {"8"};
+			return augArr;
+		case "SEP":
+			String[] sepArr= {"9"}; 
+			return sepArr;
+		case "OCT":
+			String[] octArr= {"10"}; 
+			return octArr;
+		case "NOV":
+			String[] novArr= {"11"}; 
+			return novArr;
+		case "DEC":
+			String[] decArr= {"12"}; 
+			return decArr;		
 		}
-		return 0;
+		return null;
 	}
 	
-	// Ckeck if timeUnit is day of week
-	public int isWeekDay(String timeUnit,LocalDateTime fullDate) {
-		int timeUnitToDayOfweekInt = DayOfWeek.valueOf(timeUnit).getValue();
-		int dayWeek = fullDate.get(ChronoField.DAY_OF_WEEK);
-		
-		if(timeUnitToDayOfweekInt == dayWeek)
-			return 1;
-		return 0;
-	}
-	
-	// Converts the month of fullDate to String (Mon,Tue....) and compares it with timeUnit
-	public int isMonth (String timeUnit,Month month) {
-		String newMonth = month.getDisplayName(TextStyle.SHORT, Locale.US);
-		
-		if (timeUnit.equals(newMonth))
-			return 1;
-		return 0;
-	}
-	
-	
-	// Hours of fullDate is compared to hours-range
-	public int isTimePeriod(String timeUnit,LocalDateTime fullDate) {
-		int hour = fullDate.get(ChronoField.HOUR_OF_DAY);
+	public String[] isPeriodOfDay(String timeUnit) {
 		switch (timeUnit) {
 		case "NIGHT" :
-			if(hour>=21 && hour<5)
-				return 1;
-			return 0;
+			String[] nightarr = {"21","22","23","00","01","02","03","04"};
+				return nightarr;
 		case "EARLY_MORNING" :
-			if(hour>=5 && hour<9)
-				return 1;
-			return 0;
+			String[] earlytarr = {"05","06","07","08"};
+			return earlytarr;
 		case "MORNING" :
-			if(hour>=9 && hour<13)
-				return 1;
-			return 0;
+			String[] morarr = {"09","10","11","12"};
+			return morarr;
 		case "AFTERNOON" :
-			if(hour>=13 && hour<17)
-				return 1;
-			return 0;	
+			String[] aftarr = {"13","14","15","16"};
+			return aftarr;
 		case "EVENING" :
-			if(hour>=17 && hour<21)
-				return 1;
-			return 0;
+			String[] evearr = {"17","18","19","20"};
+			return evearr;
 		}
-		return 0;
+		return null;
+	}
+
+	public int mappingRecords(HashMap<String, ArrayList<MeasurementRecord>> map,MeasurementRecord record,String unit) {
+		if(!map.containsKey(unit)) {
+			ArrayList<MeasurementRecord> list = new ArrayList<MeasurementRecord>();
+			list.add(record);
+			map.put(unit, list);
+			System.out.println("List with key "+unit +" is 1st time found.Size of new List = " + list.size() );
+			return list.size();
+		}else {
+			ArrayList<MeasurementRecord> listFromMap = map.get(unit);
+			listFromMap.add(record);
+			System.out.println("List with key "+unit +" is found with size = " + listFromMap.size() );
+			return listFromMap.size();
+		}
 	}
 	
+	// Returns the size of Arraylist that record has been added
 	@Override
 	public int add(String timeUnit, MeasurementRecord record) {
 		LocalDateTime fullDate = convertStringToDate(record.getDateTime());
+		int collecionSize=0;
+		
 		if(fullDate!=null) {
-			String timeUnitSringCheck = checkTimeUnit(timeUnit);
 			
-				
-				switch (timeUnitSringCheck) {
-				case "Season":
-					int checkIfSeason = isSeason(timeUnit, fullDate);
-					if(checkIfSeason == 1) {
-						RecordsList.add(record);
-						return RecordsList.size();
-					}
-					
-				case "Month" :
-					Month month = fullDate.getMonth();
-					int checkIfMonth = isMonth(timeUnit, month);
-					if (checkIfMonth == 1) {
-						RecordsList.add(record);
-						return RecordsList.size();
-					}
-			
-				case "Day of Week" :
-					int checkIfWeekDay = isWeekDay(timeUnit, fullDate); 
-					if(checkIfWeekDay == 1) {
-						RecordsList.add(record);
-						return RecordsList.size();
-					}
-				
-				case "DayPeriod" :
-					int checkIfTimePeriod = isTimePeriod(timeUnit, fullDate);
-					if(checkIfTimePeriod==1) {
-						RecordsList.add(record);	
-						return RecordsList.size();
-					}
+			if(isSeason(timeUnit)!=null) {
+				String[] array = isSeason(timeUnit);
+				for (int i = 0; i < array.length; i++) {
+					int arrayToInt = Integer.parseInt(array[i]);
+					if(fullDate.getMonthValue() == arrayToInt)
+							collecionSize = mappingRecords(recordsHashMap, record, array[i]);
 				}
+				return collecionSize;
+			}else if (isWeek(timeUnit)!=null) {
+				String[] array = isWeek(timeUnit);
+				int dayWeek = fullDate.get(ChronoField.DAY_OF_WEEK);
+				for (int i = 0; i < array.length; i++) {
+					int arrayToInt = Integer.parseInt(array[i]);
+					if(dayWeek== arrayToInt)
+						collecionSize = mappingRecords(recordsHashMap, record, array[i]);
+				}
+				return collecionSize;
+			}else if (isMonth(timeUnit)!=null) {
+				String[] array = isMonth(timeUnit);
+				int monthInt = fullDate.get(ChronoField.MONTH_OF_YEAR);
+				for (int i = 0; i < array.length; i++) {
+					int arrayToInt = Integer.parseInt(array[i]);
+					if(monthInt== arrayToInt)
+						collecionSize = mappingRecords(recordsHashMap, record, array[i]);
+				}
+				return collecionSize;
+			}else if (isPeriodOfDay(timeUnit)!=null) {
+				String[] array = isPeriodOfDay(timeUnit);;
+				int hour = fullDate.get(ChronoField.HOUR_OF_DAY);
+					int arrayToIntStart = Integer.parseInt(array[0]);
+					int arrayToIntStop = Integer.parseInt(array[array.length-1]);
+					if(hour>=arrayToIntStart || hour<=arrayToIntStop) {
+						collecionSize = mappingRecords(recordsHashMap, record, array[0]);
+					}
+			}
+			return collecionSize;
 		}
 		return 0;
 	}
 
-
+	// From the Arraylist that have been created with add() to each measurement , i want to make it a Hashmap with keys : timeunit
 	@Override
 	public HashMap<String, ArrayList<MeasurementRecord>> getDetailedResults() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.recordsHashMap;
 	}
 
 
 	@Override
 	public HashMap<String, Double> getAggregateMeterKitchen() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
