79b24f52783625c1787ba231f2a6bceb6cce08be
vaggelisbarb <vaggelis21aek@hotmail.com>
2019-12-15 22:59:59 +0200

    Initilaztion changes

--- src/mainengine/MainEngine.java
+++ src/mainengine/MainEngine.java
@@ -4,10 +4,10 @@
 package mainengine;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import datamodel.IResult;
 import datamodel.MeasurementRecord;
-import datamodel.ResultFactory;
 import reporting.IResultReporter;
 import reporting.TypeReportFactory;
 import timeaggregation.AggregatorFactory;
@@ -22,26 +22,14 @@ import dataload.ILoader;
 
 public class MainEngine implements IMainEngine {
 	
-	private IResult resultmanager;
-	private ResultFactory resultfactory;
-	
 	private ILoader<MeasurementRecord> dataloader;
 	private DataLoaderFactory dataloaderfactory;
 	
 	private AggregatorFactory aggregatorfactory;
 	private IAggregator aggregator; 
 	
-	private IResultReporter reporter;
-	private TypeReportFactory typereportfactory;
-	
-	private ArrayList<MeasurementRecord> objCollection;
-	
-	
 	
 	public MainEngine() {
-		objCollection = new ArrayList<MeasurementRecord>();
-		resultfactory = new ResultFactory();
-		//resultmanager = resultfactory.constructResult("ResultManager");
 		
 		dataloaderfactory = new DataLoaderFactory();
 		dataloader = dataloaderfactory.createDataLoader();
@@ -53,7 +41,7 @@ public class MainEngine implements IMainEngine {
 	@Override
 	public int loadData(String fileName, String delimeter, Boolean hasHeaderLine, int numFields,
 			ArrayList<MeasurementRecord> objCollection) {
-		return this.dataloader.load(fileName, delimeter, hasHeaderLine, numFields, this.objCollection);
+		return this.dataloader.load(fileName, delimeter, hasHeaderLine, numFields, objCollection);
 	}
 	
 	@Override
@@ -61,7 +49,7 @@ public class MainEngine implements IMainEngine {
 			String aggFunction, String description) {
 		
 		aggregator = aggregatorfactory.createAggregator(aggregatorType,aggFunction);
-		return aggregator.aggregateByTimeUnit(objCollection, aggFunction, description);
+		return aggregator.aggregateByTimeUnit(inputMeasurements, aggFunction, description);
 	}
 	
 	@Override
@@ -69,8 +57,10 @@ public class MainEngine implements IMainEngine {
 		TypeReportFactory reporterFactory = new TypeReportFactory();
 		IResultReporter reporter = reporterFactory.createReporter(reportType);
 		
-		reporter.reportResultInFile(result, filename);
+		int isOk = reporter.reportResultInFile(result, filename);
+		if(isOk<0)
+			return -1;
 		return 0;
 	}
-
+	
 }
